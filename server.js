6ab2a894b871739634a8063dd85571a3c23b2826
NamanAryan <namanaryan08@gmail.com>
2025-01-11 11:41:43 +0530

    Update CORS configuration to restrict origin to the production URL

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 
 app.use(cors({
-    origin: true,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
+    origin: 'https://urlshortner-ba26q43hq-namanaryans-projects.vercel.app/'
   }));
 
 app.get('/', (req, res) => {
